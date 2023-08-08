82a0e91e1a070d9d5124a1818b14aeb8de43d97e
Gary Hung <gary.hung@ninesmart.io>
2023-08-08 11:57:51 +0800

    Remove tsc compiled js file

--- src/deeper/module.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function default_1(message) {
-    console.log(message);
-}
-exports.default = default_1;
--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var module_1 = require("./deeper/module");
-function printThyName(opts) {
-    (0, module_1.default)(opts.name);
-}
-printThyName({ name: "Build Process 0.1.0" });
