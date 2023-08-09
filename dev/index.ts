604bbcda13a525fe4442f0541d2b2792c577549c
Gary Hung <gary.hung@ninesmart.io>
2023-08-09 17:37:27 +0800

    Don't pass module in constructor and allow mount to avoid duplicate

--- dev/index.ts
+++ dev/index.ts
@@ -2,7 +2,7 @@ import { DevServer } from 'ns-module-framework/dist/server/index';
 import { TestModule} from '~/index';
 
 const testModule = new TestModule();
-const server = new DevServer(testModule);
+const server = new DevServer();
 
 server.mount(testModule.mountingPoint);
 server.start();
